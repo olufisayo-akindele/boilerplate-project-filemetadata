adb37f336bd20ad44f76627046cec9ceb4ca98ae
olufisayo-akindele <67718162+olufisayo-akindele@users.noreply.github.com>
2023-07-06 11:05:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var cors = require('cors');
+
+const multer = require('multer');
+const upload = multer({ dest: './public/data/uploads/' });
 require('dotenv').config()
 
 var app = express();
@@ -7,14 +10,24 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+    // req.file is the name of your file in the form above, here 'uploaded_file'
+    // req.body will hold the text fields, if there were any 
+    res.json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size
+    });
 });
 
 
 
-
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, function() {
+    console.log('Your app is listening on port ' + port)
 });
